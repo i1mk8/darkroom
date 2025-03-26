26b17273db68d7e90de82fede6b9e7193c471039
Mihail <i1mk8.ln@gmail.com>
2025-03-26 11:51:27 +0500

    Добавлен класс Map, отвечающий за игоровю карту. Добавлены тесты для Map

---
+++ Darkroom.Tests/MapTests.cs
@@ -0,0 +1,43 @@
+﻿using darkroom;
+
+namespace Darkroom.Tests;
+
+[TestClass]
+public sealed class MapTests
+{
+    
+    [DataTestMethod]
+    [DataRow(100, 100, 5, 10)]
+    [DataRow(10000, 10000, 5, 10)]
+    [DataRow(100, 200, 5, 10)]
+    [DataRow(10000, 20000, 5, 10)]
+    [DataRow(100, 100, 1, 2)]
+    [DataRow(100, 100, 98, 99)]
+    public void Test(int width, int height, int minWallSize, int maxWallSize)
+    {
+        var map = Map.Generate(width, height, minWallSize, maxWallSize);
+        
+        CheckSize(map, width, height);
+        Assert.IsFalse(IsEmpty(map));
+    }
+
+    private void CheckSize(Map map, int expectedWidth, int expectedHeight)
+    {
+        Assert.AreEqual(expectedWidth, map.Width);
+        Assert.AreEqual(expectedHeight, map.Height);
+    }
+
+    private bool IsEmpty(Map map)
+    {
+        var empty = true;
+        for (var x = 0; x < map.Width; x++)
+            for (var y = 0; y < map.Height; y++)
+                if (map.Field[x, y])
+                {
+                    empty = false;
+                    break;
+                }
+        
+        return empty;
+    }
+}
---
+++ darkroom/Map.cs
@@ -0,0 +1,56 @@
+﻿namespace darkroom;
+
+public class Map(int width, int height, bool[,] field)
+{
+    public readonly int Width = width;
+    public readonly int Height = height;
+    
+    /// <summary>
+    ///  Игровое поле размером width на height
+    ///  true - в клетке находится стена
+    ///  false - в клетке пусто
+    /// </summary>
+    public readonly bool[,] Field = field;
+ 
+    public static Map Generate(int width,
+        int height,
+        int minWallSize,
+        int maxWallSize)
+    {
+        var field = new bool[width, height];
+        var random = new Random();
+        
+        var minX = width / maxWallSize != 1 ? width / (width / maxWallSize) / 2 : (width - maxWallSize) / 2;
+        var minY = height / maxWallSize != 1 ? height / (height / maxWallSize) / 2 : (height - maxWallSize) / 2;
+        var maxX = width - maxWallSize;
+        var maxY = height - maxWallSize;
+        
+        for (var startX = minX; startX < maxX; startX += maxWallSize)
+            for (var startY = minY; startY < maxY; startY += maxWallSize)
+            {
+                int endX;
+                int endY;
+                    
+                if (random.Next(0, 2) == 0)
+                {
+                    // вертикальная стена
+                    endX = startX + 1;
+                    endY = startY + random.Next(minWallSize, maxWallSize);
+                }
+                else
+                {
+                    // горизонтальная стена
+                    endX = startX + random.Next(minWallSize, maxWallSize);
+                    endY = startY + 1;
+                }
+                
+                Console.WriteLine($"X: {startX}-{endX} Y: {startY}-{endY}");
+                    
+                for (var x = startX; x < endX; x++)
+                    for (var y = startY; y < endY; y++)
+                        field[x, y] = true;
+            }
+
+        return new Map(width, height, field);
+    }
+}
