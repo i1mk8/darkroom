58a7b99b4dc91c53a5e34513ed72035c195ff410
Mihail <i1mk8.ln@gmail.com>
2025-04-28 13:24:57 +0500

    Добавлены тесты для PathFinder.

---
+++ Darkroom.Tests/PathFinderTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Drawing;
+using darkroom.model;
+
+namespace Darkroom.Tests;
+
+[TestClass]
+public class PathFinderTests
+{
+
+    [TestMethod]
+    public void FindPath_NoObstacles()
+    {
+        var map = new Map(5, 5, []);
+        var box = new RectangleF(0, 0, 1f, 1f);
+        var pathFinder = new PathFinder(map, box);
+        var start = new Point(0, 0);
+        var end = new Point(2, 0);
+
+        var path = pathFinder.FindPath(start, end);
+
+        var expectedPath = new List<Point> { new(0, 0), new(1, 0), new(2, 0) };
+        CollectionAssert.AreEqual(expectedPath, path);
+    }
+
+    [TestMethod]
+    public void FindPath_WithObstacles()
+    {
+        var walls = new List<RectangleF> { new(1.5f, 0, 1, 3) };
+        var map = new Map(5, 5, walls);
+        var box = new RectangleF(0, 0, 1f, 1f);
+        var pathFinder = new PathFinder(map, box);
+        var start = new Point(0, 0);
+        var end = new Point(3, 0);
+
+        var path = pathFinder.FindPath(start, end);
+
+        Assert.IsTrue(path.Count > 2);
+        Assert.AreEqual(start, path[0]);
+        Assert.AreEqual(end, path[^1]);
+    }
+
+    [TestMethod]
+    public void FindPath_BlockedPath()
+    {
+        var walls = new List<RectangleF> { new(0, 0, 5, 5) };
+        var map = new Map(5, 5, walls);
+        var box = new RectangleF(0, 0, 1f, 1f);
+        var pathFinder = new PathFinder(map, box);
+        var start = new Point(0, 0);
+        var end = new Point(3, 0);
+
+        var path = pathFinder.FindPath(start, end);
+
+        Assert.AreEqual(0, path.Count);
+    }
+
+    [TestMethod]
+    public void FindPath_SameStartAndEnd()
+    {
+        var map = new Map(5, 5, []);
+        var box = new RectangleF(0, 0, 1f, 1f);
+        var pathFinder = new PathFinder(map, box);
+        var point = new Point(2, 2);
+
+        var path = pathFinder.FindPath(point, point);
+
+        var expectedPath = new List<Point> { point };
+        CollectionAssert.AreEqual(expectedPath, path);
+    }
+
+    [TestMethod]
+    public void FindPath_DiagonalMovement()
+    {
+        var map = new Map(5, 5, []);
+        var box = new RectangleF(0, 0, 1f, 1f);
+        var pathFinder = new PathFinder(map, box);
+        var start = new Point(0, 0);
+        var end = new Point(2, 2);
+
+        var path = pathFinder.FindPath(start, end);
+
+        Assert.IsTrue(path.Count > 2);
+        Assert.AreEqual(start, path[0]);
+        Assert.AreEqual(end, path[^1]);
+    }
+
+    [TestMethod]
+    public void FindPath_OutOfBounds()
+    {
+        var map = new Map(5, 5, []);
+        var box = new RectangleF(0, 0, 1f, 1f);
+        var pathFinder = new PathFinder(map, box);
+        var start = new Point(-1, -1);
+        var end = new Point(6, 6);
+
+        var path = pathFinder.FindPath(start, end);
+
+        Assert.AreEqual(0, path.Count);
+    }
+}
