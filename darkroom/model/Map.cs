dc4aac4ef5fa6b40b611f9d86a5a2b994fb640d2
Mihail <i1mk8.ln@gmail.com>
2025-05-01 15:45:55 +0500

    Улучшено качество кода модели игровой карты.

--- darkroom/model/Map.cs
+++ darkroom/model/Map.cs
@@ -8,6 +8,8 @@
 /// <param name="walls">Стены, расположенные на карте</param>
 public class Map(int width, int height, List<RectangleF> walls)
 {
+    private static readonly Random Random = new();
+    
     public readonly int Width = width;
     public readonly int Height = height;
     public readonly List<RectangleF> Walls = walls;
@@ -15,76 +17,108 @@ public class Map(int width, int height, List<RectangleF> walls)
     /// <summary>
     /// Создает игровую карту
     /// </summary>
-    /// <param name="width">Длина карты</param>
-    /// <param name="height">Ширина карты</param>
+    /// <param name="mapWidth">Длина карты</param>
+    /// <param name="mapHeight">Ширина карты</param>
     /// <param name="wallOffset">Расстояние между стенами</param>
-    /// <param name="minWallSize">Минимальный размер стены (может не соблюдаться при выходе за границы карты)</param>
-    /// <param name="maxWallSize">Максимальный размер стены</param>
+    /// <param name="wallMinSize">Минимальный размер стены (может не соблюдаться при выходе за границы карты)</param>
+    /// <param name="wallMaxSize">Максимальный размер стены</param>
     /// <returns>Игровая карта</returns>
-    public static Map Generate(int width,
-        int height,
+    public static Map Generate(int mapWidth,
+        int mapHeight,
         int wallOffset,
-        int minWallSize,
-        int maxWallSize)
+        int wallMinSize,
+        int wallMaxSize)
     {
         var walls = new List<RectangleF>();
-        var random = new Random();
-
-        // Рассчитываем количество стен по горизонтали и вертикали
-        var cols = (int)Math.Ceiling((double)(width + wallOffset) / (maxWallSize + wallOffset));
-        var rows = (int)Math.Ceiling((double)(height + wallOffset) / (maxWallSize + wallOffset));
         
-        // Рассчитываем начальные отступы для центровки
-        var totalWallWidth = cols * maxWallSize + (cols - 1) * wallOffset;
-        var totalWallHeight = rows * maxWallSize + (rows - 1) * wallOffset;
-        var startOffsetX = Math.Max(0, (width - totalWallWidth) / 2);
-        var startOffsetY = Math.Max(0, (height - totalWallHeight) / 2);
+        var cols = (int)Math.Ceiling((double)(mapWidth + wallOffset) / (wallMaxSize + wallOffset));
+        var rows = (int)Math.Ceiling((double)(mapHeight + wallOffset) / (wallMaxSize + wallOffset));
+        
+        var totalWallWidth = cols * wallMaxSize + (cols - 1) * wallOffset;
+        var totalWallHeight = rows * wallMaxSize + (rows - 1) * wallOffset;
+        var startOffsetX = Math.Max(0, (mapWidth - totalWallWidth) / 2);
+        var startOffsetY = Math.Max(0, (mapHeight - totalWallHeight) / 2);
 
         for (var col = 0; col < cols; col++)
-        {
             for (var row = 0; row < rows; row++)
             {
-                // Вычисляем стартовые координаты с учетом отступов и центровки
-                var startX = startOffsetX + col * (maxWallSize + wallOffset);
-                var startY = startOffsetY + row * (maxWallSize + wallOffset);
+                var wall = GetWall(col,
+                    row,
+                    startOffsetX,
+                    startOffsetY,
+                    wallOffset,
+                    wallMinSize,
+                    wallMaxSize);
+                var normalizedWall = NormalizeWall(mapWidth, mapHeight, wall);
                 
-                // Проверяем, чтобы стена не выходила за границы карты
-                if (startX >= width || startY >= height)
+                if (normalizedWall == null)
                     continue;
                 
-                var wallSize = random.Next(minWallSize, maxWallSize + 1);
-
-                int endX;
-                int endY;
-                var wallStartX = startX;
-                var wallStartY = startY;
-                
-                if (random.Next(0, 2) == 0)
-                {
-                    // Вертикальная стена
-                    endX = startX + maxWallSize / 2;
-                    endY = startY + wallSize;
-                    wallStartX = endX - 1;
-                }
-                else
-                {
-                    // Горизонтальная стена
-                    endX = startX + wallSize;
-                    endY = startY + maxWallSize / 2;
-                    wallStartY = endY - 1;
-                }
-                
-                // Корректируем конечные координаты, если выходим за границы
-                endX = Math.Min(endX, width);
-                endY = Math.Min(endY, height);
-                
-                var wall = RectangleF.FromLTRB(wallStartX, wallStartY, endX, endY);
-                walls.Add(wall);
-                Console.WriteLine($"Wall: {wall}");
+                walls.Add(normalizedWall.Value);
+                Console.WriteLine($"Wall: {normalizedWall}");
             }
+
+        return new Map(mapWidth, mapHeight, walls);
+    }
+    
+    /// <summary>
+    /// Генерирует стену
+    /// </summary>
+    /// <param name="col">Условная колонка, в которой находится стена (иговая карта разбивается на колонки)</param>
+    /// <param name="row">Условная строка, в которой находится стена (иговая карта разбивается на строки)</param>
+    /// <param name="startOffsetX">Смещение стены по X</param>
+    /// <param name="startOffsetY">Смещение стены по Y</param>
+    /// <param name="offset">Расстояние между стенами</param>
+    /// <param name="minSize">Минимальный размер стены</param>
+    /// <param name="maxSize">Максимальный размер стены</param>
+    /// <returns>Сгенерированная стена</returns>
+    private static RectangleF GetWall(int col,
+        int row,
+        int startOffsetX,
+        int startOffsetY,
+        int offset,
+        int minSize,
+        int maxSize)
+    {
+        var startX = startOffsetX + col * (maxSize + offset);
+        var startY = startOffsetY + row * (maxSize + offset);
+        var wallSize = Random.Next(minSize, maxSize + 1);
+
+        int endX;
+        int endY;
+                
+        if (Random.Next(0, 2) == 0)
+        {
+            endX = startX + maxSize / 2;
+            endY = startY + wallSize;
+            startX = endX - 1;
+        }
+        else
+        {
+            endX = startX + wallSize;
+            endY = startY + maxSize / 2;
+            startY = endY - 1;
         }
 
-        return new Map(width, height, walls);
+        return RectangleF.FromLTRB(startX, startY, endX, endY);
+    }
+    
+    /// <summary>
+    /// Нормализует стену
+    /// </summary>
+    /// <param name="mapWidth">Длина карты</param>
+    /// <param name="mapHeight">Ширина карты</param>
+    /// <param name="wall">Стена</param>
+    /// <returns>Нормализованая стена или null, если не удалось нормализовать</returns>
+    private static RectangleF? NormalizeWall(int mapWidth, int mapHeight, RectangleF wall)
+    {
+        if (wall.Left >= mapWidth || wall.Top >= mapHeight)
+            return null;
+        
+        var right = Math.Min(wall.Right, mapWidth);
+        var bottom = Math.Min(wall.Bottom, mapHeight);
+        
+        return RectangleF.FromLTRB(wall.Left, wall.Top, right, bottom);
     }
     
     /// <summary>
@@ -104,9 +138,8 @@ public class Map(int width, int height, List<RectangleF> walls)
         if (box.Bottom > Height)
             return new RectangleF(0, Height, Width, 0);
 
-        foreach (var wall in Walls)
-            if (wall.IntersectsWith(box))
-                return wall;
+        foreach (var wall in Walls.Where(wall => wall.IntersectsWith(box)))
+            return wall;
 
         return null;
     }
@@ -116,8 +149,5 @@ public class Map(int width, int height, List<RectangleF> walls)
     /// </summary>
     /// <param name="point">Точка</param>
     /// <returns>Стена или граница, с которой пересекается точка (если пересечение существует)</returns>
-    public RectangleF? FindIntersect(PointF point)
-    {
-        return FindIntersect(new RectangleF(point.X, point.Y, 0, 0));
-    }
+    public RectangleF? FindIntersect(PointF point) => FindIntersect(new RectangleF(point.X, point.Y, 0, 0));
 }
