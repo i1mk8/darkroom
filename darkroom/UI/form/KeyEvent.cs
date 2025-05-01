a63d47d1f8af15b5f850251fcd94cb5e786b3abf
Mihail <i1mk8.ln@gmail.com>
2025-05-01 19:34:56 +0500

    Добавлена документация для обработчика нажатия клавиш.

--- darkroom/UI/form/KeyEvent.cs
+++ darkroom/UI/form/KeyEvent.cs
@@ -1,9 +1,16 @@
 ﻿namespace darkroom.UI.form;
 
+/// <summary>
+/// Обработчик нажатия клавиш
+/// </summary>
+/// <param name="actions">Действия, которые будут выполнены при нажатии клавиш</param>
 public class KeyEvent(List<KeyEventAction> actions)
 {
     private readonly List<Keys> _pressedKeys = [];
     
+    /// <summary>
+    /// Вызывается при нажатии клавиши
+    /// </summary>
     public void KeyDown(object? sender, KeyEventArgs e)
     {
         if (_pressedKeys.Contains(e.KeyCode)) 
@@ -13,13 +20,19 @@ public class KeyEvent(List<KeyEventAction> actions)
         foreach (var action in actions.Where(action => action.KeyCode == e.KeyCode && action.Single))
             action.Action();
     }
-
+    
+    /// <summary>
+    /// Вызывается при отпускании клавиши
+    /// </summary>
     public void KeyUp(object? sender, KeyEventArgs e)
     {
         if (_pressedKeys.Contains(e.KeyCode))
             _pressedKeys.Remove(e.KeyCode);
     }
     
+    /// <summary>
+    /// Выполняет действия для клавиш, удерживаемых в нажатом состоянии
+    /// </summary>
     public void Proceed()
     {
         foreach (var action in _pressedKeys.SelectMany(key=>
@@ -28,6 +41,12 @@ public class KeyEvent(List<KeyEventAction> actions)
     }
 }
 
+/// <summary>
+/// Действие, выполняемое нажатием на клавишу
+/// </summary>
+/// <param name="keyCode">Код клавиши</param>
+/// <param name="single">Флаг, указывающий, должно ли действие выполняться однократно</param>
+/// <param name="action">Действие</param>
 public class KeyEventAction(Keys keyCode, bool single, Action action)
 {
     public readonly Keys KeyCode = keyCode;
